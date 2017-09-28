4ae0dec3e8a7ac3d676671ca90b957d00e0e7d91
Philip Huschke <philiphuschke@gmail.com>
2017-09-28 14:24:10 +0200

    Fix compiler version

--- ci/install.sh
+++ ci/install.sh
@@ -13,3 +13,4 @@ export PATH=/home/travis/miniconda/bin:$PATH
 sudo apt-get -qq update
 sudo apt-get install -y libeigen3-dev libgcc-6-dev libiomp-dev swig3.0 doxygen python3-dev python3-numpy libboost-system1.55-dev libboost-filesystem1.55-dev libboost-test1.55-dev libboost-mpi1.55-dev libopenblas-dev libmetis-dev libmumps-seq-dev libann-dev libarpack2-dev
 sudo apt-get install -y lcov curl texlive-font-utils libglu1-mesa-dev
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 99
